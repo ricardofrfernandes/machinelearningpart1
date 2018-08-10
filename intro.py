cd044235bc824a6e47a14adbfb3a9bea6452b68c
Ricardo Fernandes <ricardo.fernandes@valuedate.pt>
2018-08-10 16:28:25 +0100

    Added train.csv file locally

--- intro.py
+++ intro.py
@@ -5,7 +5,6 @@ from sklearn import model_selection
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
